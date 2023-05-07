a8b55c48b439d3e85ea2daee438573e334776e45
FlorescuMiruna <mirunastefaniaflorescu@gmail.com>
2023-05-07 22:04:33 +0300

    Book page

--- bookspace/src/app/components/book/book.component.ts
+++ bookspace/src/app/components/book/book.component.ts
@@ -6,5 +6,29 @@ import { Component } from '@angular/core';
   styleUrls: ['./book.component.css']
 })
 export class BookComponent {
+  isRead: boolean = false;
+  averageRating: number = 0;
+  isInTBR: boolean = false;
+  calculateClasses1() {
+    if (this.isRead === false)
+      return 'btn btn-outline-success';
+    else
+      return 'btn btn-success';
+  }
+
+
+  calculateClasses2() {
+    if (this.isInTBR === false)
+      return 'btn btn-outline-dark';
+    else
+      return 'btn btn-dark';
+  }
+  addMovieToTBR() {
+    this.isInTBR = !this.isInTBR;
+
+  }
+  addBook() {
+    this.isRead =  !this.isRead;
+  }
 
 }
--- bookspace/src/app/components/home/home.component.ts
+++ bookspace/src/app/components/home/home.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthenticationService } from 'src/app/service/authentication.service';
+import { BookService } from 'src/app/service/book.service';
+
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+
+  constructor(private bokService: BookService, private authenticationService: AuthenticationService, private router:Router) { }
+  ngOnInit(): void {
+  }
+
+  goToBook() {
+
+
+    this.router.navigate(['/book'])
+
+
+
+  }
 
 }
