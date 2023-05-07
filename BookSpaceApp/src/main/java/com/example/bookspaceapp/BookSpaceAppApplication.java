5807ac472fc9d05038e00733846579d1d20ff044
FlorescuMiruna <mirunastefaniaflorescu@gmail.com>
2023-05-07 19:07:58 +0300

    Backend setup

---
+++ BookSpaceApp/src/main/java/com/example/bookspaceapp/BookSpaceAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.bookspaceapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookSpaceAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookSpaceAppApplication.class, args);
+    }
+
+}
---
+++ BookSpaceApp/src/test/java/com/example/bookspaceapp/BookSpaceAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bookspaceapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookSpaceAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
